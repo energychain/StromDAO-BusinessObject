2f8337a072cd7f787e1a524d08baf54420491383
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-04-24 11:53:33 +0200

    Initial Checkin

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # StromDAO-BusinessObject
 Abstract BusinessObject for StromDAO Energy Blockchain. Abstraction layer between blockchain technology and business logic providing energy market related entities and use cases.
+
+## Entities 
+
+### SmartMeter [Reading]
+
+### GatewayAdministrator [GWALink]
+
+### PowerDelivery [PowerDelivery]
+
+### BalanceGroup [Stromkonto Proxy]
+
+### Account/Balance [Stromkonto]
+
+## Contributing
+- https://stromdao.de/
+- https://gitter.im/stromdao/Lobby
---
+++ test/smartmeter.js
@@ -0,0 +1,68 @@
+/**
+  Smart Meter related Use Cases
+*/
+
+var assert = require('assert');
+var StromDAONode = require("../StromDAONode.js");    
+
+describe('SmartMeter', function() {
+	this.timeout(30000);
+	
+	var known_gwalink = '0x119AA4A3C2C7287f99FCBB41C5a78a8Dc15d1338';
+	var external_id = Math.random()*10000000; 
+	
+	var node = new StromDAONode.Node({external_id:external_id});
+	
+	it('Write via Reader/Read via GWALink', function() {
+			var myReading = new Date().getTime();
+			node.gwalink(known_gwalink).then(
+					function(gwalink) {
+							var reader_in = gwalink.reader_in;
+							reader_in.pingReading(myReading).then(function(o) {
+									var interval = setInterval(function() {
+										node.provider.getBlockNumber().then(function(blockNumber) {
+												if(typeof block1 == "undefined") var block1=blockNumber;
+												var block2=blockNumber;													
+												if(block1!=block2) {
+														clearInterval(interval);
+														gwalink.obj.zss(node.wallet.address).then(
+															function(o) {
+																assertEqual(o[1].toString(),myReading);																
+																done(); 
+															}
+														);
+												
+												}
+										});
+									}
+									,1000);
+							});
+			});
+	});
+	it('Write via Reader/Read via Reader', function() {
+			var myReading = new Date().getTime();
+			node.gwalink(known_gwalink).then(
+					function(gwalink) {
+							var reader_in = gwalink.reader_in;
+							reader_in.pingReading(myReading).then(function(o) {
+									var interval = setInterval(function() {
+										node.provider.getBlockNumber().then(function(blockNumber) {
+												if(typeof block1 == "undefined") var block1=blockNumber;
+												var block2=blockNumber;													
+												if(block1!=block2) {
+														clearInterval(interval);
+														reader_in.obj.readings(node.wallet.address).then(
+															function(o) {
+																assertEqual(o[1].toString(),myReading);																
+																done(); 
+															}
+														);
+												
+												}
+										});
+									}
+									,1000);
+							});
+			});
+	});
+});
