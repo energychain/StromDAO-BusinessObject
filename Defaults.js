179497984cbc84ba663c00e527b8ec317ec73caf
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-05-15 19:21:03 +0200

    Changed Default Behaviour

--- Defaults.js
+++ Defaults.js
@@ -1,12 +1,12 @@
 this.deployment=function(options) {
 				var contract_type=[];
-				contract_type["StromDAO-BO.sol_DSO"]="0xD3F73Ef3939bb284646F88b4b2b47d5c7F4D7bC1";
-				contract_type["StromDAO-BO.sol_MPO"]="0xCEaf534d3be165Be54D393D0c5548DA7f589467a";
-				contract_type["StromDAO-BO.sol_Provider"]="0x66bBCF6e888AA672DDF3DD80217C23f22f7633Fe";
+				contract_type["StromDAO-BO.sol_DSO"]="0x3D8bB3f14e332ae56bf97E2BddB24a3E1c87bcfb";
+				contract_type["StromDAO-BO.sol_MPO"]="0xa037ce97716720C27D67E65Bfcb61b5D315410be";
+				contract_type["StromDAO-BO.sol_Provider"]="0x23b0209Bc95dc71f8fc29e526C438C8F4332E4e6";
 				
-				contract_type["StromDAO-BO.sol:DSO"]="0xD3F73Ef3939bb284646F88b4b2b47d5c7F4D7bC1";
-				contract_type["StromDAO-BO.sol:MPO"]="0xCEaf534d3be165Be54D393D0c5548DA7f589467a";
-				contract_type["StromDAO-BO.sol:Provider"]="0x66bBCF6e888AA672DDF3DD80217C23f22f7633Fe";
+				contract_type["StromDAO-BO.sol:DSO"]="0x3D8bB3f14e332ae56bf97E2BddB24a3E1c87bcfb";
+				contract_type["StromDAO-BO.sol:MPO"]="0xa037ce97716720C27D67E65Bfcb61b5D315410be";
+				contract_type["StromDAO-BO.sol:Provider"]="0x23b0209Bc95dc71f8fc29e526C438C8F4332E4e6";
 				
 				var roles=[];
 				roles[0]=0;
---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODOs
+
+- Add Default Contracts to RoleLookup
+- Add RoleLookup to Chain Genesis (Requires Chain Reset) on StromDAO Node
+- Create Test cases for Events / Event System
+- Check if Auto-Subscribe for StromkontoProxy is possible to speed up deployments
+
