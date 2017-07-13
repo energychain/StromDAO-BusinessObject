2f8e849904ca467dec49c267271d05858e2cc40a
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-07-13 13:21:18 +0200

    Added PeerHistory function to Stromkonto

--- Stromkonto.js
+++ Stromkonto.js
@@ -137,6 +137,57 @@ this.stromkonto = function(obj_or_address) {
 							});
 							return p2;
 						};
+						instance.historyPeer=function(address_meterpoint,address_account,length) {							
+							var p2 = new Promise(function(resolve2, reject2) { 
+								
+								parent.rpcprovider.getBlockNumber().then(function(latest_block) {
+									if(length==0) length=latest_block;
+									
+									parent.wallet.provider.getLogs({address:obj_or_address,fromBlock:latest_block-length,toBlock:latest_block}).then(							
+									function(logs) {															
+											entries=[];
+											for(var i=0;i<logs.length;i++) {
+													var data = logs[i].data;
+													if(data.length>256) {
+														data=data.substr(2);
+														_from ="0x"+ split64(data).substr(26);
+														data=data.substr(64);
+														_to ="0x"+split64(data).substr(26);
+														data=data.substr(64);
+															
+														_value =(split64(data));
+														data=data.substr(64);
+														_base =(split64(data));
+														data=data.substr(64);
+														_fromSoll =(split64(data));
+														data=data.substr(64);
+														_fromHaben =(split64(data));
+														data=data.substr(64);
+														_toSoll =(split64(data));
+														data=data.substr(64);
+														_toHaben =(split64(data));
+														data=data.substr(64);
+														if(((_from.toLowerCase()==address_meterpoint.toLowerCase())||(_to.toLowerCase()==address_meterpoint.toLowerCase()))&&(((_from.toLowerCase()==address_account.toLowerCase())||(_to.toLowerCase()==address_account.toLowerCase())))) {
+															var entry={};
+															entry.from=_from;
+															entry.to=_to;
+															entry.base=_base;
+															entry.value=_value;
+															entry.toSoll=_toSoll;
+															entry.toHaben=_toHaben;
+															entry.fromSoll=_fromSoll;
+															entry.fromHaben=_fromHaben;
+															entry.blockNumber=logs[i].blockNumber;
+															entries.push(entry);
+														}
+													}
+											}
+											resolve2(entries);
+									});
+								});
+							});
+							return p2;
+						};
 						resolve(instance);
 			});
 			return p1;
