7d0248081f34d5fcb58b93a882507c68adcea11d
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-02-22 17:20:52 +0100

    Adding new files for SPV Handling (Hybrid Power Market Related)

---
+++ SPV.js
@@ -0,0 +1,203 @@
+/**
+ * StromDAO Business Object: DirectConnectionFactory
+ * =========================================
+ * Factory Contract to build direct connections handling for StromDAO Energy Blockchain.
+ * 
+ * @author Thorsten Zoerner thorsten.zoerner@stromdao.de 
+ * */
+ 
+
+this.spv = function(obj_or_address) {
+			if(typeof obj_or_address == "undefined") obj_or_address=parent.options.contracts["StromDAO-BO.sol_SPV"];
+			
+			var p1 = new Promise(function(resolve, reject) { 
+				var instance=parent._objInstance(obj_or_address,'StromDAO-BO.sol_SPV');			
+				instance.test = {};				
+				
+				instance.fund=function(uint256_value) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.fund(uint256_value).then(function(o) {
+										parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				instance.allowFunding=function() {							
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.allowFunding().then(function(o) {													
+										parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				instance.disallowFunding=function() {							
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.disallowFunding().then(function(o) {													
+											parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				instance.spend=function(uint256_value) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.spend(uint256_value).then(function(o) {
+										parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				
+				instance.earn=function(uint256_value) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.earn(uint256_value).then(function(o) {
+										parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				
+				instance.meteredEarn=function(uint256_reading) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.meteredEarn(uint256_reading).then(function(o) {
+										parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				
+				instance.meteredPrice=function(uint256_value) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+											instance.obj.meteredPrice(uint256_value).then(function(o) {
+													resolve2(o.hash);
+											});
+								});
+								return p2;
+				};
+				
+				instance.sell=function(address_to,uint256_fund,uint256_value) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.sell(address_to,uint256_fund,uint256_value).then(function(o) {
+											parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				
+				instance.transfer=function(address_to, uint256_fund) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+									instance.obj.transfer(address_to, uint256_fund).then(function(o) {
+										parent._waitForTransactionKeepRef(o,resolve2);	
+									});
+						});
+						return p2;
+				};
+				
+				instance.totalSupply=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.totalSupply().then(function(o) {	
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};
+				
+				instance.owner=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.owner().then(function(o) {														
+								 resolve2(o[0].toString());									
+							});									
+					});
+					return p2;
+				};
+				
+				instance.balanceOf=function(address_account) {					
+						var p2 = new Promise(function(resolve2, reject2) { 											
+											instance.obj.balanceOf( parent._resolveName(address_account)).then(function(o) {
+													resolve2(o[0].toString()*1);
+											});
+								});
+								return p2;
+				};
+					
+				instance.totalSupply=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.totalSupply().then(function(o) {																				
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};
+				
+				instance.sumHolders=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.sumHolders().then(function(o) {														
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};
+				
+				instance.earnings=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.earnings().then(function(o) {														
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};
+				instance.lastreading=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.lastreading().then(function(o) {														
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};	
+				instance.name=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.name().then(function(o) {														
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};		
+				instance.stromkonto=function() {					
+					var p2 = new Promise(function(resolve2, reject2) { 							
+							instance.obj.stromkonto().then(function(o) {														
+								 resolve2(o[0].toString()*1);									
+							});									
+					});
+					return p2;
+				};											
+				/**
+				 * Stores a reading to this contract instance. Requires sender to be approved Meter-Point 
+				 * @see approveMP()
+				 */
+				instance.issue= function() {	
+							
+					var p2 = new Promise(function(resolve2, reject2) { 
+							var bdx="";
+							
+							instance.obj.issue({value:"0x0",gasPrice:"0x0",gasLimit:3903918}).then(function(o) {									
+								parent._waitForTransactionKeepRef(o,resolve2);									
+							});									
+					});
+					return p2;
+				};
+				
+				instance.addMeterpoint= function(address_meterpoint) {	
+							
+					var p2 = new Promise(function(resolve2, reject2) { 
+							var bdx="";
+							
+							instance.obj.addMeterpoint( parent._resolveName(address_meterpoint),{value:"0x0",gasPrice:"0x0",gasLimit:3903918}).then(function(o) {									
+								parent._waitForTransactionKeepRef(o,resolve2);									
+							});									
+					});
+					return p2;
+				};
+				
+				resolve(instance);
+			});
+			return p1;
+		};
---
+++ test/spv.js
@@ -0,0 +1,121 @@
+/**
+  StromDAO Business Object - SPV testing for Energy Blockchain
+*/
+
+var assert = require('assert');
+var StromDAONode = require("../StromDAONode.js");    
+
+console.log("STROMDAO - Special Purpose Vehilce for Hybrid Power Market");
+console.log("-----------------------------------------------------------");
+console.log("Storyline:");
+console.log("This sample case illustrates how to do a community based");
+console.log("invest into a solar plant and mange this via energy ");
+console.log("blockchain. All tests get issued live as the story goes.");
+console.log("-----------------------------------------------------------");
+
+var spv_address="";
+var owner_id = "SPV_"+Math.random()*10000000; 
+var owner_address="";
+
+var investor_id = "SPV_"+Math.random()*10000000;
+var investor_address="";
+	
+describe('Setup Owner Role for Hybrid Power Market', function() {
+	this.timeout(300000);
+	
+	var node_owner = {};
+	var known_rolelookup = '0x0000000000000000000000000000000000000006';
+		
+	it('Create a new entity that takes role of project owner', function(done) {		
+		node_owner = new StromDAONode.Node({external_id:owner_id,testMode:true});								
+		assert.equal(node_owner.wallet.address.length,42);
+		console.log("> Address (Owner)",node_owner.wallet.address);
+		owner_address=node_owner.wallet.address;
+		done();							
+	});				
+	
+    it('Use SPV Factory to create SPV entity ', function(done) {		
+		node_owner.spvfactory().then(function(spvf) {
+			 spvf.build("0x19BF166624F485f191d82900a5B7bc22Be569895","Unit Test").then(function(o) {
+					spv_address=o;
+					assert.equal(spv_address.length,42);	
+					console.log("> Address (SPV)",spv_address);
+					done();	
+				});
+		});
+	});				
+	
+	it('As Owner: Allow funding', function(done) {				
+		node_owner.spv(spv_address).then(function(spv) {			 
+			 spv.allowFunding().then(function(o) {											
+					assert.equal(o.length,66);							
+					done();	
+				});
+		});
+	});
+	
+	it('As Owner: Do initial funding of 100', function(done) {		
+		node_owner.spv(spv_address).then(function(spv) {
+			 spv.fund(100).then(function(o) {						
+					assert.equal(o.length,66);							
+					done();	
+				});
+		});
+	});
+	it('SPV total funded is 100', function(done) {		
+		node_owner.spv(spv_address).then(function(spv) {
+			 spv.totalSupply().then(function(o) {										
+					assert.equal(o,100);							
+					done();	
+				});
+		});
+	});		
+});	
+	
+describe('Become second investor', function() {
+	this.timeout(300000);
+	var node_investor = {};
+	var known_rolelookup = '0x0000000000000000000000000000000000000006';
+		
+	it('Create a new entity that takes role of second Investor', function(done) {		
+		node_investor = new StromDAONode.Node({external_id:investor_id,testMode:true});						
+		assert.equal(node_investor.wallet.address.length,42);
+		investor_address=node_investor.wallet.address;
+		console.log("> Address (Investor)",node_investor.wallet.address);
+		done();							
+	});		
+	
+	it('As Investor: Do funding of 200', function(done) {		
+		node_investor.spv(spv_address).then(function(spv) {
+			 spv.fund(200).then(function(o) {						
+					assert.equal(o.length,66);							
+					done();	
+				}).catch(function(e) {console.log("Error",e);});
+		});
+	});
+	
+	it('SPV total funded is 300', function(done) {		
+		node_investor.spv(spv_address).then(function(spv) {
+			 spv.totalSupply().then(function(o) {										
+					assert.equal(o,300);							
+					done();	
+				});
+		});
+	});	
+	it('- owner has 100', function(done) {		
+		node_investor.spv(spv_address).then(function(spv) {
+			 spv.balanceOf(owner_address).then(function(o) {										
+					assert.equal(o,100);							
+					done();	
+				});
+		});
+	});	
+	it('- Investor has 200', function(done) {		
+		node_investor.spv(spv_address).then(function(spv) {
+			 spv.balanceOf(investor_address).then(function(o) {										
+					assert.equal(o,200);							
+					done();	
+				});
+		});
+	});		
+});
